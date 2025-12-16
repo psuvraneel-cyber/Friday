2b71465924ea7e5a0c48f218752b390291d50b3b
Suvraneel674 <psuvraneel@gmail.com>
2025-12-16 18:38:15 +0530

    5th commit

--- Friday/settings.py
+++ Friday/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'Friday.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'Templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
